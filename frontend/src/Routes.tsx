fab36e56f6ccff0a25d5424735b70fd0a1245f22
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2025-02-16 15:51:29 -0400

    frontend: rutas actualizadas

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
-import CourseDetails from "./components/CourseDetails";
 import Home from "./pages/home/home";
 import Teachers from "./pages/teachers/teachers";
+import Course from "./pages/course/course";
+import Courses from "./pages/courses/courses";
 
 const AppRoutes = () => {
   return (
@@ -10,8 +11,9 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="/courses/1" element={<CourseDetails />} />
+          <Route path="/courses/1" element={<Course />} />
           <Route path="/teachers" element={<Teachers />} />
+          <Route path="/courses" element={<Courses />} />
         </Route>
       </Routes>
     </Router>
